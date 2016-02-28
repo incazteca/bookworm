6cf16a1c5f2b64360170a8252051fe361939cb31
Franco Reyes <freyes1988@gmail.com>
2016-02-28 08:32:51 +0000

    Implementing file too large failure

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-// Look into using multipart/form-data
+const _10MB_LIMIT int64 = 1000 * 10000
 
-type File struct {
-	body string
+type successRes struct {
+	fileContent    string         `json:"file_content"`
+	totalWordCount int            `json:"total_word_count"`
+	wordCounts     map[string]int `json:"word_counts"`
 }
 
-func fileHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Wazzup")
+type failRes struct {
+	error string `json:"error"`
+}
+
+// Look into using multipart/form-data
+
+func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == "POST" {
+		if r.ContentLength > _10MB_LIMIT {
+			w.WriteHeader(http.StatusRequestEntityTooLarge)
+			failure := failRes{"File Upload is limited to 10MB. Please submit a smaller file"}
+			json.NewEncoder(w).Encode(failure)
+		}
+	} else {
+		http.Error(w, "The requested page is not available", http.StatusNotFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/file/upload", fileHandler)
+	http.HandleFunc("/file/upload", fileUploadHandler)
 	http.ListenAndServe(":8080", nil)
 }
