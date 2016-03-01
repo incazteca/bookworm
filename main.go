c4ea9583b2fee6851b737714883f7827dded76d7
Franco Reyes <freyes1988@gmail.com>
2016-03-01 03:48:16 +0000

    Refactor main.go

--- main.go
+++ main.go
@@ -18,33 +18,29 @@ type FailRes struct {
 	ErrorMsg string `json:"error"`
 }
 
-// TODO: Clean this up, It's a disgrace...
-
 func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	if r.Method == "POST" {
 		if r.ContentLength > _10MB_LIMIT {
-			w.WriteHeader(http.StatusRequestEntityTooLarge)
-			response := FailRes{"File Upload is limited to 10MB. Please submit a smaller file"}
-			json.NewEncoder(w).Encode(response)
+			writeError(w, "File upload is limited to 10MB. Please submit a smaller file",
+				http.StatusRequestEntityTooLarge)
 		} else {
 			r.ParseMultipartForm(_10MB_LIMIT)
 			file, _, err := r.FormFile("file")
+
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				failure := FailRes{err.Error()}
-				json.NewEncoder(w).Encode(failure)
+				writeError(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 			defer file.Close()
 
 			output, err := bookworm.Parse(file, r.FormValue("filter"))
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				failure := FailRes{err.Error()}
-				json.NewEncoder(w).Encode(failure)
+				writeError(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+
 			w.WriteHeader(http.StatusOK)
 			response := SuccessRes{output.Body, output.TotalWordCount, output.WordCounts}
 			json.NewEncoder(w).Encode(response)
@@ -52,12 +48,18 @@ func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 	} else {
-		failure := FailRes{"Page not found"}
-		json.NewEncoder(w).Encode(failure)
+		writeError(w, "Page not found", http.StatusNotFound)
+		return
 	}
 }
 
 func main() {
 	http.HandleFunc("/file/upload", fileUploadHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func writeError(w http.ResponseWriter, errMessage string, errCode int) {
+	w.WriteHeader(errCode)
+	failure := FailRes{errMessage}
+	json.NewEncoder(w).Encode(failure)
 }
